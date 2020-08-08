80eb1e8e2732f5ea7f73d0d5738207301dba1b10
Ram Vinoth <rponnarasu@silver-peak.com>
2020-08-08 16:13:21 -0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('sass/banner.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+</head>
+<body>
+    @include('layouts.banner')
+    <div id="app">
+        <main>
+        </main>
+    </div>
+    
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.0.1/dist/alpine.js" defer></script>
+</body>
+</html>
